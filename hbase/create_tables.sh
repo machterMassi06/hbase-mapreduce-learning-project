5828193605d90299a3522e51e3b973b498e2678a
machterMassi06 <massinissa.machter@ijclab.in2p3.fr>
2026-06-12 23:21:57 +0200

    add script to create hbase_table

---
+++ hbase/create_tables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 <HBASE_HOME>"
+  exit 1
+}
+
+# Check parameter
+if [ -z "$1" ]; then
+  usage
+fi
+
+HBASE_HOME="$1"
+TABLE_PREFIX="web_site"
+
+# Maybe in future: salting & pre-splitting (for real distributed HBase cluster)
+
+COMMAND="create '${TABLE_PREFIX}.visits', {NAME => 'info', COMPRESSION => 'LZ4'}"
+
+echo -e "$COMMAND" | "$HBASE_HOME/bin/hbase" shell -n
