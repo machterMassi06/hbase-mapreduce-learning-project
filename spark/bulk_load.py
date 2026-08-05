0f072e0ecde4db63f9433e726064c33d9bf8f5b3
machterMassi06 <massinissa.machter@ijclab.in2p3.fr>
2026-08-05 14:39:31 +0200

    refact bulk load py script

--- spark/bulk_load.py
+++ spark/bulk_load.py
@@ -1,54 +1,9 @@
 import json
 import sys
+
 from spark_utils.session import create_spark_session
 from pyspark.sql import functions as F
 
-def to_thin_row(row, columns):
-    """
-    Convert a Spark Row into HBase "thin bulk load" format.
-
-    In the thin bulk load model, each row is represented as:
-        (rowkey, [(family, qualifier, value), ...])
-
-    This is more efficient than the basic bulk load model because:
-        - all columns of a row are grouped together
-        - reduces shuffle overhead in Spark
-        - improves HFile generation performance
-
-    Args:
-        row: Spark Row object (input record)
-        columns (dict): catalog column mapping:
-            {
-                column_name: {
-                    "cf": column_family,
-                    "col": qualifier,
-                    "type": data_type
-                }
-            }
-
-    Returns:
-        tuple:
-            (rowkey, list of (family, qualifier, value))
-
-    """
-
-
-    rowkey = row["key"]
-
-    family_qual_values = []
-
-    for col_name, meta in columns.items():
-
-        if col_name == "key":
-            continue
-
-        cf = meta["cf"]
-        qualifier = meta["col"]
-        value = row[col_name]
-
-        family_qual_values.append((cf, qualifier, value))
-
-    return (rowkey, family_qual_values)
 
 def thin_bulk_load(catalog_path, input_csv, output_path):
     """
@@ -57,13 +12,12 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
     This pipeline follows the official HBase Spark integration model:
         1. Load HBase catalog (JSON)
         2. Build Spark DataFrame
-        3. Transform data into thin row format:
-               (rowkey, [(cf, qualifier, value), ...])
-        4. Use hbaseBulkLoadThinRows to generate HFiles
+        4. Use hbaseBulkLoadThinRows java wrapper to generate HFiles
         5. Load HFiles into HBase using LoadIncrementalHFiles
 
     Args:
         catalog_path (str): path to HBase Table JSON catalog definition
+        input_csv : data source file 
         output_path (str): HDFS/local path where HFiles will be generated
 
     Returns:
@@ -74,10 +28,9 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
     sc = spark.sparkContext
     jvm = sc._jvm
 
-
     # Java classes
     TableName = jvm.org.apache.hadoop.hbase.TableName
-    HBaseContext = jvm.org.apache.hadoop.hbase.spark.HBaseContext
+
 
     # Load catalog
     with open(catalog_path, "r") as f:
@@ -86,16 +39,19 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
     table_name = catalog["table"]["name"]
     columns = catalog["columns"]
 
-    # Example dataset csv (replace with real source : parquet)
-    df = spark.read \
-        .option("header", True) \
-        .option("inferSchema", True) \
+    # Load CSV example ((replace with real source : parquet))
+
+    df = (
+        spark.read
+        .option("header", True)
+        .option("inferSchema", True)
         .csv(input_csv)
+    )
 
-
-    # ordering by rowkey is required for HFile generation
+    # Build HBase rowkey
 
     if table_name == "web_site.visits":
+
         df = df.withColumn(
             "key",
             F.concat_ws(
@@ -106,40 +62,72 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
             )
         )
 
+
     elif table_name == "web_site.users":
+
         df = df.withColumn(
             "key",
-            F.col("user_id")
+            F.col("user_id").cast("string")
         )
+
 
     else:
-        raise Exception(f"No rowkey strategy defined for table {table_name}")
 
+        raise Exception(
+            f"No rowkey strategy defined for {table_name}"
+        )
+
+
+    # ordering by rowkey is required for HFile generation
     df = df.orderBy("key")
 
-    # Broadcast variables for distributed execution
-    bc_columns = sc.broadcast(columns)
 
-    # Transform DataFrame into thin row RDD
-    # _jrdd to JavaRdd
-    rdd = df.rdd.map(
-        lambda row: to_thin_row(
-            row,
-            bc_columns.value
+    # Convert catalog Python dict to Java Map
+
+    java_columns = jvm.java.util.HashMap()
+
+
+    for col_name, meta in columns.items():
+
+        java_meta = jvm.java.util.HashMap()
+
+        java_meta.put(
+            "cf",
+            meta["cf"]
         )
-    )
+
+        java_meta.put(
+            "col",
+            meta["col"]
+        )
+
+        java_meta.put(
+            "type",
+            meta.get("type", "string")
+        )
+
+        java_columns.put(
+            col_name,
+            java_meta
+        )
+
 
     # Initialize HBase context
     hbase_conf = sc._jsc.hadoopConfiguration()
     hbase_context = jvm.org.apache.hadoop.hbase.spark.HBaseContext(sc._jsc.sc(), hbase_conf, None)
 
-    # Execute Thin Bulk Load (generate HFiles in "thin" mode without using MapReduce Jobs)
-    hbase_context.hbaseBulkLoadThinRows(
-        rdd._jrdd,
-        TableName.valueOf(table_name),
-        # Mapping function applied to each RDD element
-        # Input: (rowkey, [(cf, qualifier, value), ...]), Output: same structure, sometimes normalized
-        lambda t: (t[0], t[1]),
+    # Call my Java wrapper
+
+    wrapper = (
+        jvm.mapreduce.hbase.ThinBulkLoadWrapper
+    )
+
+    # Execute Thin Bulk Load (generate HFiles in "thin")
+    wrapper.bulkLoadThinRows(
+        hbase_context,
+        df._jdf,
+        java_columns,
+        table_name,
 
         # Temporary directory where HFiles will be generated (staging area before loading into HBase)
         output_path,
@@ -155,9 +143,14 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
         256 * 1024 * 1024
     )
 
+
     # Load generated HFiles into HBase
-    conn = hbase_context.hbaseConnection()
+
+    connection_factory = jvm.org.apache.hadoop.hbase.client.ConnectionFactory
+    conn = connection_factory.createConnection(hbase_conf)
+
     admin = conn.getAdmin()
+
     if not admin.tableExists(TableName.valueOf(table_name)):
         raise ValueError(f"Table {table_name} does not exist in HBase!")
 
@@ -165,6 +158,7 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
     region_locator = conn.getRegionLocator(TableName.valueOf(table_name))
 
     load = jvm.org.apache.hadoop.hbase.mapreduce.LoadIncrementalHFiles(hbase_conf)
+
     load.doBulkLoad(
         jvm.org.apache.hadoop.fs.Path(output_path),
         admin,
@@ -175,7 +169,8 @@ def thin_bulk_load(catalog_path, input_csv, output_path):
     spark.stop()
 
 
-# CLI ENTRYPOINT
+
+
 if __name__ == "__main__":
     """
 
